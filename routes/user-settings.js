71ca95f481154d0f76af6ae45e3a0e7d4d8db2e0
Ian Gilmore <igilmore@ucsd.edu>
2015-03-04 19:15:41 -0800

    fixed crashing

--- routes/user-settings.js
+++ routes/user-settings.js
@@ -1,4 +1,6 @@
 var path = require('path');
+var Parse = require('parse').Parse;
+Parse.initialize('8ND8FWpNrWD1j2zkGymXBFAGWebC7xiuA2GT7zAk', 'tYcMRGV7XEpjFv782VzQ2ezItHVuU40vsCMZ71DU');
 
 module.exports = function(req, res) {
 	var query = new Parse.Query(Parse.User);
@@ -8,12 +10,13 @@ module.exports = function(req, res) {
 		success: function(user) {
 			console.log(user);
 			var data = {
-						id: user.id,
-						myUsername: user.getUsername(),
-						myUserPic: user.get('userPic'),
-						email: user.get('email'),
+				id: user.id,
+				myUsername: user.getUsername(),
+				myUserPic: user.get('userPic'),
+				email: user.get('email')
 			};
+			console.log(data);
+			require(path.join(__dirname, 'header')).renderPage(req, res, 'user-settings');
 		}
-
-	require(path.join(__dirname, 'header')).renderPage(req, res, 'user-settings');
+	});		
 };
