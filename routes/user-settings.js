9dcd9e46522c4e90cc1bbcd7517a51005a7465d0
Ian Gilmore <igilmore@ucsd.edu>
2015-03-04 17:32:00 -0800

    added user profile image + tweaked settings page

--- routes/user-settings.js
+++ routes/user-settings.js
@@ -1,5 +1,19 @@
 var path = require('path');
 
 module.exports = function(req, res) {
+	var query = new Parse.Query(Parse.User);
+
+	query.equalTo('username', req.params.user);
+	query.first({
+		success: function(user) {
+			console.log(user);
+			var data = {
+						id: user.id,
+						myUsername: user.getUsername(),
+						myUserPic: user.get('userPic'),
+						email: user.get('email'),
+			};
+		}
+
 	require(path.join(__dirname, 'header')).renderPage(req, res, 'user-settings');
 };
--- routes/users.js
+++ routes/users.js
@@ -14,6 +14,7 @@ module.exports = function(req, res) {
 					var data = {
 						id: user.id,
 						myUsername: user.getUsername(),
+						myUserPic: user.get('userPic'),
 						email: user.get('email'),
 						reviews: []
 					};
